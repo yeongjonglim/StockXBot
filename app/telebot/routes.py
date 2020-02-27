c9e2f0625883c7b3b19642b3948ba231f2b7d3d8
voltendron <voltendron@gmail.com>
2020-02-27 15:27:47 +0800

    updated chat and message error handling

--- app/telebot/routes.py
+++ app/telebot/routes.py
@@ -19,16 +19,18 @@ def setupWebhook():
 def receivedMessage():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), telegram_bot)
+    print(update)
 
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
 
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    print("Got text message: ", text)
+    if chat_id and msg_id:
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8').decode()
+        print("Got text message: ", text)
 
-    queried_company = Company.query.filter_by(stock_name=text).first()
-    queried_announcements = Announcement.query.filter_by(announced_company=queried_company).order_by(Announcement.id).limit(3).all()
-    sent_status = send_announcement(queried_announcements, chat_id=chat_id)
+        queried_company = Company.query.filter_by(stock_name=text).first()
+        queried_announcements = Announcement.query.filter_by(announced_company=queried_company).order_by(Announcement.id).limit(3).all()
+        sent_status = send_announcement(queried_announcements, chat_id=chat_id)
 
     return 'ok', 200
