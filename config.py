e597903c880bf4a3c198db40f164784c4157f9cc
voltendron <voltendron@gmail.com>
2020-04-22 22:01:07 +0800

    updated config requirements

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config(object):
     # App default config
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'StockXBot'
 
+    # Logging config
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT') or None
+
     # DB config
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
