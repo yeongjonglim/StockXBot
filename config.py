6551abfcc3a252573254bf223e18b5f4a2229fb6
voltendron <voltendron@gmail.com>
2020-05-17 16:53:05 +0800

    Removed unnecessary information

--- config.py
+++ config.py
@@ -21,4 +21,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['mystockxbot@gmail.com']
+    ADMINS = [os.environ.get('ADMIN_EMAIL')]
